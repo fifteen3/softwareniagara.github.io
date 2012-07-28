a4cb3566853a5933f89a8ca53981e84ef9768857
Nickolas Kenyeres <nkenyeres@gmail.com>
2012-07-27 20:33:50 -0400

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Softwareniagara::Application.config.session_store :cookie_store, key: '_softwareniagara_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Softwareniagara::Application.config.session_store :active_record_store
