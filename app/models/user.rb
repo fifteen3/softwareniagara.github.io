54f0b4456f8f57e3a254019aa8dfc7d477f31e6d
Nickolas Kenyeres <nkenyeres@gmail.com>
2012-08-02 14:51:54 -0400

    Users can't register

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User
   rolify
 
   # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
+  # :token_authenticatable, :confirmable, :registerable
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 
   ## Database authenticatable
