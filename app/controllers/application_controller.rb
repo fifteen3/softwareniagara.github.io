468d5ce7baa2158b01803cf9ff0f652f8005ad10
Nickolas Kenyeres <nkenyeres@gmail.com>
2012-08-03 00:06:38 -0400

    Use admin layout for login form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  layout :layout_by_resource
+  
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
+  end
+
+  protected
+
+  def layout_by_resource
+    if devise_controller?
+      'admin/application'
+    end
   end
 end
