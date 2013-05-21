09afecac312e3129d393a57c82dc3b0b1ea5e580
Nickolas Kenyeres <nkenyeres@gmail.com>
2013-05-20 20:59:25 -0400

    Removed waypoint scripting.

--- src/documents/scripts/script.js
+++ src/documents/scripts/script.js
@@ -7,26 +7,6 @@
   $doc.ready(function() {
     $doc.foundation();
 
-    $('#header-menu').waypoint(function(direction) {
-       if (direction === 'down') {
-         $('#toolbar-menu li').each(function() {
-           var $this = $(this);
-           if ($this.hasClass('toggle-visibility')) {
-             $this.removeClass('show-for-small');
-           }
-         });
-       } else {
-         $('#toolbar-menu li').each(function() {
-           var $this = $(this);
-           if ($this.hasClass('toggle-visibility')) {
-             $this.addClass('show-for-small');
-           }
-         });
-       }
-    }, {
-      offset: -30
-    });
-
     // Hide address bar on mobile devices (except if #hash present, so we don't mess up deep linking).
     if (Modernizr.touch && !window.location.hash) {
       $(window).load(function () {
