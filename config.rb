0253f8901b29ce6b6cef1d104474fe79ae308067
Nickolas Kenyeres <nkenyeres@gmail.com>
2013-05-31 20:14:55 -0400

    Setup project to use compass/sass

---
+++ config.rb
@@ -0,0 +1,30 @@
+# Require any additional compass plugins here.
+
+http_path       = '/'
+css_dir         = 'assets/css'
+sass_dir        = '_sass'
+images_dir      = 'assets/images'
+javascripts_dir = 'assets/js'
+
+# compressed output in production environment
+environment  = :production
+output_style = (environment == :production) ? :compressed : :expanded
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+# Leave this alone!
+preferred_syntax = :scss
+
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+environment = :production
+output_style = :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
