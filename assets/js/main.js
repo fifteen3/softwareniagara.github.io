284c34e66a004d37fa43ac753e32b279efb479d0
Nickolas Kenyeres <nkenyeres@gmail.com>
2013-07-01 16:47:56 -0400

    Converting to a single page site.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,12 @@
-(function() {
+(function($) {
     $(document).foundation();
-    $(document).ready(function() {
 
+    $(document).ready(function() {
+        $('[data-scroll]').smoothScroll({
+            offset: -106,
+            beforeScroll: function() {
+                $('.dropdown').removeClass('open').removeAttr('style');
+            }
+        });
     });
-})();
+})($);
