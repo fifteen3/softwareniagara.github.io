aa8c0aacd88537bdb44139341b4c107ceab6f75d
Nickolas Kenyeres <nkenyeres@gmail.com>
2013-07-17 00:07:02 -0400

    Big cleanup. Still requires mobile and scripting.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,8 @@
 (function($) {
     $(document).foundation();
 
+
     $(document).ready(function() {
-        $('[data-scroll]').smoothScroll({
-            offset: -106,
-            beforeScroll: function() {
-                $('.dropdown').removeClass('open').removeAttr('style');
-            }
-        });
+        $.scrollIt();
     });
 })($);
