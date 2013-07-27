dc3e9158f339606b596a01e8c5e5115630574d83
Nickolas Kenyeres <nkenyeres@gmail.com>
2013-07-26 21:16:33 -0400

    Refactoring assets; removing unused things.

--- config.rb
+++ config.rb
@@ -15,17 +15,7 @@ output_style = (environment == :production) ? :compressed : :expanded
 relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 # Leave this alone!
 preferred_syntax = :scss
-
-
-# You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
-
-environment = :production
-output_style = :compressed
-
-# To enable relative paths to assets via compass helper functions. Uncomment:
-relative_assets = true
